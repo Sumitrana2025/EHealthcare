55158f4c9e3b8f394711c2ac51597e1931818604
Sumit Rana <ranasumit258@gmail.com>
2022-10-12 16:00:07 +0530

    Initial5

--- API/Data/Migrations/20221011103323_BasketEntityAdded.Designer.cs
+++ API/Data/Migrations/20221011103323_BasketEntityAdded.Designer.cs
@@ -2,6 +2,7 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20221011103323_BasketEntityAdded")]
+    partial class BasketEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.9");
@@ -22,8 +24,8 @@ namespace API.Data.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
 
-                    b.Property<string>("BuyerId")
-                        .HasColumnType("TEXT");
+                    b.Property<int>("BuyerId")
+                        .HasColumnType("INTEGER");
 
                     b.HasKey("Id");
 
